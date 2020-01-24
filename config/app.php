0f59710b61a02119e78311862aa5bbd8094708d5
alirezamohamadi <alirezamkco@gmail.com>
2020-01-24 18:09:35 +0330

    AdsTableSeeder created and db update

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     |
     */
-
+    'faker_locale' => 'fa_IR',
     'name' => env('APP_NAME', 'Laravel'),
 
     /*
